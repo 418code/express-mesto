a7a55885fdfe8b2331acc0c1cea566acb0ed68a2
Maxim Tolstokory <maxim.code@outlook.com>
2021-12-25 16:23:46 -0900

    refactor: update CORS origin

--- utils/utils.js
+++ utils/utils.js
@@ -56,7 +56,7 @@ module.exports.limiterValues = {
 };
 
 module.exports.corsOptions = {
-  origin: 'http://localhost:3001',
+  origin: 'http://mesto.418co.de',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204,
