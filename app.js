a51fb7a01f31939bcea9aef4c082bcdbf75312b5
Maxim Tolstokory <maxim.code@outlook.com>
2022-01-19 16:27:53 -0900

    refactor: move limiter to separate module

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const rateLimit = require('express-rate-limit');
 const cors = require('cors');
-const {
-  limiterValues,
-  corsOptions,
-} = require('./utils/utils');
+const { corsOptions } = require('./utils/utils');
 const { handleErrors } = require('./middlewares/error');
 const {
   requestLogger,
   errorLogger,
 } = require('./middlewares/logger');
 const router = require('./routes/index');
+const limiter = require('./middlewares/limit');
 
 const { PORT } = process.env;
 
@@ -20,17 +17,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-const limiter = rateLimit({
-  windowMs: limiterValues.windowMs,
-  max: limiterValues.max, // limit each IP to max requests per windowMs
-});
-
 const app = express();
 
-app.use(cors(corsOptions));
-app.use(express.json()); // body-parser is built-in with latest express
 app.use(requestLogger); // log incoming requests excluding body
 app.use(limiter); // basic ddos prevention
+app.use(cors(corsOptions));
+app.use(express.json()); // body-parser is built-in with latest express
 app.use(router);
 app.use(errorLogger); // log errors before handling it
 app.use(handleErrors);
